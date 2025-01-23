6142742a02d407f6b6c867eab1e2eb1f275bae12
alexZ7000 <78627928+alexZ7000@users.noreply.github.com>
2025-01-23 15:15:31 -0300

    :sparkles: feat: adding about page

--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -1,12 +1,14 @@
 import Navbar from "@components/Navbar";
 import Hero from "@components/Hero";
 import Footer from "@components/Footer";
+import About from "@components/About.tsx";
 
 export default function Home() {
     return (
         <div className="app">
             <Navbar />
             <Hero />
+            <About />
             <Footer />
         </div>
     );
